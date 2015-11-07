c8f324cb89eb51364fa6752af199067a66e8b9cc
Alex Comba <alex.comba@agilebg.com>
2015-11-07 11:41:37 +0100

    Up to Widgets Basics

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -3,11 +3,45 @@ openerp.oepetstore = function(instance, local) {
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
 
-    local.HomePage = instance.Widget.extend({
-        start: function() {
-            console.log("pet store home page loaded");
+    var MyClass = instance.web.Class.extend({
+        init: function(name) {
+            this.name = name;
+        },
+        say_hello: function() {
+            console.log("hello", this.name);
         },
     });
+    var MySpanishClass = MyClass.extend({
+        say_hello: function() {
+            this._super();
+            console.log("translation in Spanish: hola", this.name);
+        },
+    });
+    var my_object = new MySpanishClass("Alex")
+    my_object.say_hello()
+
+    local.GreetingsWidget = instance.Widget.extend({
+        className: 'oe_petstore_greetings',
+        init:function(parent, name) {
+            this._super(parent);
+            this.name = name;
+        },
+        start: function() {
+            this.$el.append("<div>We are so happy to see you again in this menu!</div>");
+            console.log(this.getParent().$el);
+        },
+    });
+
+    local.HomePage = instance.Widget.extend({
+        className: 'oe_petstore_homepage',
+        start: function() {
+            this.$el.append("<div>Hello dear Odoo user!</div>");
+            var greeting = new local.GreetingsWidget(this);
+            console.log(this.getChildren()[0].$el);
+            return greeting.appendTo(this.$el);
+        },
+    });
+
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
 }
